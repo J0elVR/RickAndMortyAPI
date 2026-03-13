700c6c2ac2a4e33d7c7d91db6c3e845a576461af
Joel Villa <joelvr2k@icloud.com>
2026-03-12 18:05:35 -0600

    Initial Commit

---
+++ RickAndMortyAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyAPI
+//
+//  Created by Joel Villa on 12/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
